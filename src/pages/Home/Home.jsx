5f7872929dfccd5339c6d1f30e650e5daf988acd
nishaj0 <njnishaj0@gmail.com>
2023-10-27 22:10:08 +0530

    added some reusable classes and styles

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,9 +2,12 @@ import React from "react";
 
 import "./home.css";
 import "../../containers/Header/Header";
+import Header from "../../containers/Header/Header";
 
 function Home() {
-   return <div className="mcq__home"></div>;
+   return <div className="mcq__home">
+    <Header />
+   </div>;
 }
 
 export default Home;
