2b58e414c8d4ecc237083f50dffea57c14ca804e
nishaj0 <njnishaj0@gmail.com>
2023-10-27 22:10:18 +0530

    created header

--- src/containers/Header/Header.jsx
+++ src/containers/Header/Header.jsx
@@ -1,12 +1,22 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
-import './header.css'
+import "./header.css";
 
 function Header() {
-  return (
-    <div className="mcq__header">
-    </div>
-  )
+   return (
+      <div className="mcq__header ">
+         <div className="mcq__header-title">
+            <Link to="/">
+               <h2>MCQ Game</h2>
+            </Link>
+         </div>
+         <div className="mcq__header-links">
+            <a href="#">Help</a>
+            <a href="#">About</a>
+         </div>
+      </div>
+   );
 }
 
-export default Header
+export default Header;
